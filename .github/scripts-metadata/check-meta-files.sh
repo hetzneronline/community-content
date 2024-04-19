7957653412ee3de6b928eb0687e0a625f4e70b73
svenja.michal <svenja.michal@hetzner-cloud.de>
2024-04-19 11:12:54 +0200

    Add GitHub Actions » Automatic issues (annual review) & check metadata in PRs

---
+++ .github/scripts-metadata/check-meta-files.sh
@@ -0,0 +1,32 @@
+markdown_file="$1"
+json_schema=".github/scripts-metadata/schema.json"
+output_file=".github/scripts-metadata/meta-converted.json"
+validate_file=".github/scripts-metadata/validation_result.txt"
+variables=".github/scripts-metadata/variables.txt"
+new_variables=".github/scripts-metadata/new-variables.txt"
+updated_variables=".github/scripts-metadata/updated-variables.txt"
+
+echo $markdown_file
+
+# Extract YAML front matter from Markdown file and turn it into a JSON file
+metadata=$(awk '/^---$/{f=!f;next}f' "$markdown_file" | yq eval -o=json - > "$output_file")
+
+# Validate metadata against the JSON schema
+npx ajv-cli validate -s "$json_schema" -d "$output_file" --all-errors --errors=line > "$validate_file" 2>&1
+
+# Extract error messages from "validate_file"
+cat "$validate_file" | tail -n +2 | jq -r '.[] | "\(.instancePath | ltrimstr("/"))=\(.message)"'  > "$new_variables"
+
+# Check if any keys are missing
+for key in SPDX-License-Identifier path slug date title short_description tags author author_link author_img author_description language available_languages header_img cta; do
+  if ! jq -e ".[\"$key\"]" $output_file > /dev/null; then
+    echo "$key=Not found" >> $new_variables
+  fi
+done
+
+# Replace the original variable messages with error messages
+awk -F'=' 'NR==FNR{a[$1]=$2; next} {if($1 in a) {$2=a[$1]} print}' OFS='=' "$new_variables" "$variables" > "$updated_variables"
+
+# Update formatting to make it work for `source` and `envsubst` in `.guthub/workflows/check-metadata-in-files.yml`
+sed -i -e 's/SPDX-License-Identifier/license/g' -e "s/^\(.*\)=\(.*\)$/\\1='\2'/g" -e 's/^/export /' "$updated_variables"
+echo "$updated_variables"
---
+++ .github/scripts-metadata/summary-table.md
@@ -0,0 +1,19 @@
+## Error messages for $markdown_file
+
+| Header 1                | Header 2             |
+| ----------------------- | -------------------- |
+| SPDX-License-Identifier | $license             |
+| path                    | $path                |
+| slug                    | $slug                |
+| date                    | $date                |
+| title                   | $title               |
+| short_description       | $short_description   |
+| tags                    | $tags                |
+| author                  | $author              |
+| author_link             | $author_link         |
+| author_img              | $author_img          |
+| author_description      | $author_description  |
+| language                | $language            |
+| available_languages     | $available_languages |
+| header_img              | $header_img          |
+| cta                     | $cta                 |
