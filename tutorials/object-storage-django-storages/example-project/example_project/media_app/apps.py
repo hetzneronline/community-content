2a8ef5e5d0f4eb7a5ab361bb51924ac692b871ca
svenja.michal <svenja.michal@hetzner-cloud.de>
2025-05-06 12:38:23 +0200

    Spelling and formatting updates

--- tutorials/object-storage-django-storages/example-project/example_project/media_app/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class MediaAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'media_app'
-    verbose_name = 'Media App'
